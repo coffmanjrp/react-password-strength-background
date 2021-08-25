0c6b9bfac72db70fd316bf2089ad840bd0f155cb
coffmanjrp <coffmanjrp@gmail.com>
2021-08-25 10:51:15 -0700

    Implement bg blur effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [blur, setBlur] = useState(20);
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setBlur(20 - password.length * 2);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({ email, password });
+  };
+
   return (
     <div className="App">
-      <div className="background" />
-      <div className="bg-white rounded p-10 text-center shadow-md">
+      <div className="background" style={{ filter: `blur(${blur}px)` }} />
+      <form
+        className="bg-white rounded p-10 text-center shadow-md"
+        onSubmit={(e) => handleSubmit(e)}
+      >
         <h1 className="text-3xl">Image Password Strength</h1>
         <p className="text-sm text-gray-700">
           Change the password to see the effect
@@ -18,7 +37,10 @@ function App() {
             name="email"
             id="email"
             placeholder="Enter Email"
+            autoComplete="off"
             className="border block w-full p-2 mt-2 rounded"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="my-4 text-left">
@@ -30,7 +52,10 @@ function App() {
             name="password"
             id="password"
             placeholder="Enter Password"
+            autoComplete="off"
             className="border block w-full p-2 mt-2 rounded"
+            value={password}
+            onChange={(e) => handlePassword(e)}
           />
         </div>
         <button
@@ -39,7 +64,7 @@ function App() {
         >
           Submit
         </button>
-      </div>
+      </form>
     </div>
   );
 }
